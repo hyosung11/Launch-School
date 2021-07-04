600408594dc548d52e791486f71bb61253c41d6b
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-04 17:57:28 -0400

    Submit exercise 2 solution

--- Courses/JS100/Book/Input-Output/greeter.js
+++ Courses/JS100/Book/Input-Output/greeter.js
@@ -6,8 +6,20 @@
 // What is your name? Sue
 // Hello, Sue!
 
+/*
 let readlineSync = require('readline-sync');
 let name = readlineSync.question("What is your name? ");
 console.log(`Hello, ${name}!`);
+*/
 
-// 2. 
+// 2. Modify the greeter.js program to ask for the user's first and last names separately, then greet the user with their full name.
+
+// $ node greeter.js
+// What is your first name? Sue
+// What is your last name? Roberts
+// Hello, Sue Roberts!
+
+let readlineSync = require('readline-sync');
+let firstName = readlineSync.question('What is your first name? ');
+let lastName = readlineSync.question('What is your last name? ');
+console.log(`Hello, ${firstName} ${lastName}!`);
