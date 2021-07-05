3747239b566867222028dcd85b79dda87e6be479
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-05 08:46:43 -0400

    Submit Functions Exercise 2 solution for greeter.js

--- Courses/JS100/Book/Input-Output/greeter.js
+++ Courses/JS100/Book/Input-Output/greeter.js
@@ -19,7 +19,22 @@ console.log(`Hello, ${name}!`);
 // What is your last name? Roberts
 // Hello, Sue Roberts!
 
-let readlineSync = require('readline-sync');
-let firstName = readlineSync.question('What is your first name? ');
-let lastName = readlineSync.question('What is your last name? ');
+// let readlineSync = require('readline-sync');
+// let firstName = readlineSync.question('What is your first name? ');
+// let lastName = readlineSync.question('What is your last name? ');
+// console.log(`Hello, ${firstName} ${lastName}!`);
+
+// Functions Exercise 2
+// In the exercises for the previous chapter, you wrote a dynamic greeter program named greeter.js. Add a function to this program that solicits the user's first and last names in separate invocations; the function should return the appropriate name as a string. Use the return values to greet the user with their full name.
+
+function getName(prompt) {
+  let readlineSync = require('readline-sync');
+  let name = readlineSync.question(prompt);
+  return name;
+}
+
+let firstName = getName('What is your first name? ');
+let lastName = getName('What is your last name? ');
 console.log(`Hello, ${firstName} ${lastName}!`);
+
+
