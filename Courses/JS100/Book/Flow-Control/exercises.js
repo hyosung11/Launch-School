990bfa0aa6a83f410b4e86ac9950a59a529f1dbb
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-05 17:16:55 -0400

    Submit exercise 8 another example

--- Courses/JS100/Book/Flow-Control/exercises.js
+++ Courses/JS100/Book/Flow-Control/exercises.js
@@ -163,13 +163,14 @@ function numberRange(number) {
   } else if (number <= 50) {
     console.log(`${number} is between 0 and 50`);
   } else if (number <= 100) {
-    console.log(`${number} is between 50 and 100`);
+    console.log(`${number} is between 51 and 100`);
   } else {
     console.log(`${number} is greater than 100 `);
   }
 }
 
-numberRange(25);
-numberRange(75);
-numberRange(125);
-numberRange(-25);
+numberRange(25); // => 25 is between 0 and 50
+numberRange(75); // => 75 is between 51 and 100
+numberRange(125); // => 125 is greater than 100
+numberRange(-25); // -25 is less than 0
+numberRange(3231); // => 3231 is greater than 100
