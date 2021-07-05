c5d40d697a3813eb027948d57031a26f5d84a86d
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-05 16:14:16 -0400

    Submit exercise 4 solution for switch statement

--- Courses/JS100/Book/Flow-Control/exercises.js
+++ Courses/JS100/Book/Flow-Control/exercises.js
@@ -57,6 +57,47 @@ function evenOrOdd(number) {
   }
 }
 
-evenOrOdd('tasty')
-evenOrOdd(1.55)
-evenOrOdd(45)
+// evenOrOdd('tasty')
+// evenOrOdd(1.55)
+// evenOrOdd(45)
+
+// 4. What does the following code log to the console, and why?
+
+function barCodeScanner(serial) {
+  switch (serial) {
+    case "123":
+      console.log("Product1");
+    case "113":
+      console.log("Product2");
+    case "142":
+      console.log("Product3");
+    default:
+      console.log("Product not found!");
+  }
+}
+
+// barCodeScanner("113");
+// Product2
+// Product3
+// Product not found!
+
+// Since the case clauses of the switch statement lack break statements, control falls through from the matching case '113' clause and executes the code in the case '142' and default clauses as well. JavaScript doesn't care that the criteria for extra case clauses don't match our serial value. This fall-through behavior is often undesirable. To avoid it, you must add break statements to each case clause:
+
+// refactor with break statements
+function barCodeScanner(serial) {
+  switch (serial) {
+    case "123":
+      console.log("Product 1");
+      break;
+    case "113":
+      console.log("Product 2");
+      break;
+    case "142":
+      console.log("Product 3");
+      break;
+    default:
+      console.log("Product not found!");
+  }
+}
+
+barCodeScanner('142')
