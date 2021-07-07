04555ac8c56d26ec06a357022e20995d04618dd7
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-07 16:30:42 -0400

    Submit exercise 3 - objectt that behaves as an array solution

--- Courses/JS100/Book/Objects/exercises.js
+++ Courses/JS100/Book/Objects/exercises.js
@@ -21,3 +21,32 @@
 // 'true'
 
 // All the listed values are valid keys. Note, though, that JavaScript coerces the non-string key values to strings. Given the listed values, 1 and '1' represent the same key, as do true and 'true'. This equivalency will bite you at some point; it's inevitable, so be ready.
+
+// 3. Use object literal syntax (e.g., { key: value, ... } notation) to create an object that behaves as an array in a for statement. The object should contain at least 3 elements. You should place your code between the braces in the let statement:
+
+// my solution
+let familyArray = {
+  0: 'HyoSung',
+  1: 'Sohee',
+  2: 'Omi',
+  3: 'SungOh',
+  length: 4,
+};
+
+for (let i = 0; i < familyArray.length; i += 1) {
+  console.log(familyArray[i]);
+}
+
+// Launch School solution
+let myArray = {
+  0: 'a',
+  1: 'b',
+  2: 'c',
+  length: 3,
+};
+
+for (let i = 0; i < myArray.length; i += 1) {
+  console.log(myArray[i]);
+}
+
+// Our array-like object isn't a perfect mimic of a regular JavaScript array, however. In particular, it doesn't modify the length property when you add or delete elements. It also doesn't support methods like forEach, filter, and push.
