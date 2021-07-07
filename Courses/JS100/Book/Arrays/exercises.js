cc2c7defc1b7d2c1054541174c8c21d51a059730
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-07 10:42:53 -0400

    Submit exercise 3 `nested arrays` solutions

--- Courses/JS100/Book/Arrays/exercises.js
+++ Courses/JS100/Book/Arrays/exercises.js
@@ -33,14 +33,15 @@
 
 // 2. Log all of the even values from myArray to the console.
 
-let myArray = [1, 3, 6, 11, 4, 2, 4, 9, 17, 16, 0];
+// let myArray = [1, 3, 6, 11, 4, 2, 4, 9, 17, 16, 0];
 
-for (let i = 0; i < myArray.length; i += 1) {
-  let value = myArray[i];
-  if (value % 2 === 0) {
-    console.log(value);
-  }
-}
+
+// for (let i = 0; i < myArray.length; i += 1) {
+//   let value = myArray[i];
+//   if (value % 2 === 0); {
+//     console.log(value);
+//   }
+// }
 
 // 6
 // 4
@@ -53,10 +54,42 @@ for (let i = 0; i < myArray.length; i += 1) {
 
 // Use `forEach` and let JS take care of the indexing:
 
-myArray.forEach(function(value) {
-  if (value % 2 ===0) {
-    console.log(value)
-  }
+// myArray.forEach(function(value) {
+//   if (value % 2 === 0) {
+//     console.log(value)
+//   }
+// });
+
+// 3. Let's make the problem a little harder. In this problem, we're again interested in even numbers, but this time the numbers are in nested arrays in a single outer array.
+
+let myArray = [
+  [1, 3, 6, 11],
+  [4, 2, 4],
+  [9, 17, 16, 0],
+];
+
+
+// for (let i = 0; i < myArray.length; i += 1) {
+//   for (let j = 0; j < myArray[i].length; j += 1) {
+//     let value = myArray[i][j];
+//     if (value % 2 === 0) {
+//       console.log(value);
+//     }
+//   }
+// }
+
+// The approach is straightforward, but it's a bit verbose. However, the chained brackets in `myArray[i][j]` make it visually explicit that we're dealing with a two-dimensional nested array.
+
+// As before, we can also use forEach to abstract away the messy details of indexes and stopping conditions:
+
+myArray.forEach(function (nestedArray) {
+  nestedArray.forEach(function(value) {
+    if (value % 2 === 0) {{
+      console.log(value);
+    }}
+  });
 });
+
+
 
 
