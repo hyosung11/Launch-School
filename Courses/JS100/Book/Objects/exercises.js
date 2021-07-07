d443bcef487a6e26ca28ef15410f0fff4de33478
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-07 16:54:41 -0400

    Submit exercise 6 - identify the value type

--- Courses/JS100/Book/Objects/exercises.js
+++ Courses/JS100/Book/Objects/exercises.js
@@ -85,3 +85,16 @@ let myProtoObj = {
 };
 
 let myObj = Object.create(myProtoObj);
+
+// 6. Which of the following values are primitive values? Which are objects? Which are neither?
+
+// "foo" - primitive
+// 3.1415 - primitive
+// [ 'a', 'b', 'c' ] - object (arrays are objects)
+// false - primitive
+// foo - identifier (Identifiers are used to name things that have values, such as variables and functions, but they are not values by themselves. Thus, they are neither primitive values nor objects.)
+// function bar() { return "bar"; } - object (functions are objects)
+// undefined - primitive
+// { a: 1, b: 2 } - object
+
+
