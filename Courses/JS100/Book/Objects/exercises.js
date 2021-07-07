8b227e1c874879b76e13483ae863e07149761ea5
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-07 17:45:27 -0400

    Submit exercise 9 - objects mutable, string and other primitives not mutable

--- Courses/JS100/Book/Objects/exercises.js
+++ Courses/JS100/Book/Objects/exercises.js
@@ -159,20 +159,44 @@ let objToCopy = {
 // console.log(newObj3);       // => { bar: 2 }
 
 // Solution
-function copyObj(sourceObject, keys) {
-  let destinationObject = {};
+// function copyObj(sourceObject, keys) {
+//   let destinationObject = {};
 
-  if(keys) {
-    keys.forEach(function(key) {
-      destinationObject[key] = sourceObject[key];
-    });
+//   if(keys) {
+//     keys.forEach(function(key) {
+//       destinationObject[key] = sourceObject[key];
+//     });
 
-    return destinationObject;
-  } else {
-    return Object.assign(destinationObject, sourceObject);
-  }
-}
+//     return destinationObject;
+//   } else {
+//     return Object.assign(destinationObject, sourceObject);
+//   }
+// }
 
-console.log(objToCopy);
+// console.log(objToCopy);
 
 // I need to study this one a few times to understand it.
+
+// 9. What does the following program log to the console? Why?
+
+let foo = {
+  a: 'hello',
+  b: 'world',
+};
+
+let qux = 'hello';
+
+function bar(argument1, argument2) {
+  argument1.a = 'hi';
+  argument2 = 'hi';
+}
+
+bar(foo, qux);
+
+console.log(foo.a); // => hi
+console.log(qux); // => hello
+
+// Solution
+// The program logs 'hi' and 'hello' The reason behind this is that objects are mutable; strings and other primitives are not. Also, variable reassignment, such as that on line 10, doesn't mutate the original object even when the object is mutable. Thus, line 9 mutates the foo by assigning its a property to a new value ('hi'). Therefore, the code on line 15 logs hi. On the other hand, line 10 reassigns the argument2 variable, but it doesn't mutate the string represented by qux. Thus, line 16 logs hello: the original value of the qux variable.
+
+
