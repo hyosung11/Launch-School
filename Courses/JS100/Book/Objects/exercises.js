699d206e32aad9398c4382a6f30dcfbfd0b4aa84
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-07 16:20:46 -0400

    Submit exercise 2 - valid keys solution

--- Courses/JS100/Book/Objects/exercises.js
+++ Courses/JS100/Book/Objects/exercises.js
@@ -2,13 +2,22 @@
 
 // 1. Given the following code, how can you access the name of the person?
 
-let person = {
-  name: 'Bob',
-  occupation: 'web developer',
-  hobbies: 'painting',
-};
+// let person = {
+//   name: 'Bob',
+//   occupation: 'web developer',
+//   hobbies: 'painting',
+// };
 
-console.log(person.name); // => Bob
-console.log(person['name']); // => Bob
+// console.log(person.name); // => Bob
+// console.log(person['name']); // => Bob
 
+// 2. Which of the following values are valid keys for an object?
 
+// 1
+// '1'
+// undefined
+// 'hello world'
+// true
+// 'true'
+
+// All the listed values are valid keys. Note, though, that JavaScript coerces the non-string key values to strings. Given the listed values, 1 and '1' represent the same key, as do true and 'true'. This equivalency will bite you at some point; it's inevitable, so be ready.
