5a66566142d3d92101c619f83d2a2752f189cc32
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-08 10:28:42 -0400

    Submit exercise 3 - 7 solutions

--- Courses/JS100/Book/More-Stuff/exercises.js
+++ Courses/JS100/Book/More-Stuff/exercises.js
@@ -5,7 +5,7 @@
 let array1 = [1, 2, 3];
 let array2 = array1;
 array1[1] = 4;
-console.log(array2); // => [1, 4, 3]
+// console.log(array2); // => [1, 4, 3]
 
 // This result demonstrates that `array1` and `array2` reference the same array: if we change an element using `array1`, it also changes that element in `array2`. The opposite is also true: if we change an element in `array2`, that also changes the element in `array1`.
 
@@ -42,3 +42,66 @@ array2 // => [ 1, 4, 3 ]
 // More specifically, line 6 in the output tells you that a ReferenceError exception occurred and that the name greeting isn't defined. Line 7 repeats some earlier information: JavaScript detected the error at column 15 of line 4 of the program, but it also tells you that the code is in the hello function. Line 8 tells you that hello was called from line 13 of the program in an anonymous function, namely the global-level of the program.
 
 // The rest of the output comes from running the code in node and probably isn't useful to you as an application programmer.
+
+// 3. Write some code to output the square root of 37.
+
+// console.log(Math.sqrt(37)) // => 6.082762530298219
+
+// 4. Given a list of numbers, write some code to find and display the largest numeric value in the list.
+
+// List            | Max
+// 1, 6, 3, 2      | 6
+// -1, -6, -3, -2	| -1
+// 2, 2	          | 2
+
+// console.log(Math.max(1, 6, 3, 2));
+// console.log(Math.max(-1, -6, -3, -2))
+// console.log(Math.max(2, 2))
+
+// 5. What does the following function do?
+
+function doSomething(string) {
+  return string
+    .split(' ')
+    .reverse()
+    .map((value) => value.length);
+}
+
+// console.log(doSomething('Pursuit of happiness')) // => [ 5, 3, 6, 7 ] the lengths of each word in reverse order
+
+// 6. Write a function that searches an array of strings for every element that matches the regular expression given by its argument. The function should return all matching elements in an array.
+
+// Example
+let words = [
+  'laboratory',
+  'experiment',
+  'flab',
+  'Pans Labyrinth',
+  'elaborate',
+  'polar bear',
+];
+ // Solution 1
+function allMatches(words, pattern) {
+  let matches = [];
+  for (let index = 0; index < words.length; index += 1) {
+    if (pattern.test(words[index])) {
+      matches.push(words[index]);
+    }
+  }
+  
+  return matches;
+}
+
+console.log(allMatches(words, /lab/)); // => ['laboratory', 'flab', 'elaborate']
+
+// Solution 2
+function allMatches(words, pattern) {
+  return words.filter((word) => pattern.test(word));
+}
+
+console.log(allMatches(words, /lab/)); // => ['laboratory', 'flab', 'elaborate']
+
+// 7. What is exception handling and what problem does it solve?
+
+// Exception handling is a process that deals with errors in a manageable and predictable manner. It uses the try/catch statement to catch exceptions that the code in the try block raises, and lets the programmer deal with the problem in a way that makes sense and perhaps prevents a catastrophic failure or nasty bug.
+
