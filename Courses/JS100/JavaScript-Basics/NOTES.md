7a70245e0024388acb8f2bc11f3a947f4c3908a0
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-08 19:33:14 -0400

    Write Loops notes

--- Courses/JS100/JavaScript-Basics/NOTES.md
+++ Courses/JS100/JavaScript-Basics/NOTES.md
@@ -270,3 +270,188 @@ TypeError: tweet.length is not a function
 `length` is called without parentheses. This is because it is a string _property_, not a string _method_.
 
 `tweet.length()`is a type mismatch: `tweet.length` is expected to be a number, but we use it as a function.
+
+## Loops
+
+### Loop and Log
+
+```js
+for (let i = 0; i <= 10; i += 2) {
+  // include your code here
+  console.log(i)
+}
+
+0
+2
+4
+6
+8
+10
+
+for ([initialExpression]; [condition]; [incrementExpression]) {
+  statement
+}
+```
+
+### Countdown
+
+```js
+for (let i = 10; i > 0; i -= 1) {
+  console.log(i);
+}
+
+console.log('Launch!');
+
+// alternative
+for (let i = 10; i >= 0; i -= 1) {
+  if (i === 0) {
+    console.log('Launch!');
+  } else {
+    console.log(i);
+  }
+}
+```
+
+### Triple Greeting
+
+Write a loop that logs `greeting` three times.
+
+```js
+// for loop
+let greeting = 'Aloha!';
+
+for (let count = 1; count <= 3; count += 1) {
+  console.log(greeting);
+}
+
+// while loop
+let greeting = 'Aloha!'
+let count = 1;
+
+while (count <= 3) {
+  console.log(greeting);
+  count += 1;
+}
+```
+
+### Take Two
+
+Write a `for` loop that iterates over all numbers from 1 to 100, and outputs the result of multiplying each element by 2.
+
+```js
+for (let num = 1; num <= 100; num += 1) {
+  console.log(num * 2);
+}
+```
+
+### Looping over Array Elements
+
+Using the code below as a starting point, write a `while` loop that logs the elements of `array` at each index, and terminates after logging the last element of the array.
+
+```js
+let array = [1, 2, 3, 4];
+let index = 0;
+
+while (index < array.length) {
+  console.log(array[index]);
+  index += 1;
+}
+```
+
+### Continue
+
+Write a `for` loop that loops over the elements of the array `cities` and logs the length of each string to the console. If the element is `null`, skip forward to the next iteration without logging anything to the console.
+
+```js
+let cities = ['Istanbul', 'Los Angeles', 'Tokyo', null, 'Vienna', null, 'London', 'Beijing', null];
+
+for (let i = 0; i < cities.length; i += 1) {
+  if (cities[i] === null) {
+    continue;
+  }
+
+  console.log(cities[i].length);
+}
+
+8
+11
+5
+6
+6
+7
+```
+
+`continue` terminates the current iteration and continues with execution of the next iteration. This allows us to skip each element that is equal to `null`.
+
+### And on and on and on
+
+```js
+// loops forever - condition left empty; JS treats as true
+for (let i = 0; ; i += 1) {
+  console.log("and on");
+}
+
+// terminate the loop with break statement
+for (let i = 0; ; i += 1) {
+  console.log("and on");
+  break;
+}
+```
+### That's Odd
+
+Write a while loop that logs all odd natural numbers between 1 and 40.
+
+```js
+let num = 1;
+
+while (num < 40) {
+  console.log(num);
+  num += 2;
+}
+```
+
+### Finding Nemo
+
+Loop over the elements of the array `fish`, logging each one. Terminate the loop immediately after logging the string `'Nemo'`.
+
+```js
+let fish = ['Dory', 'Marlin', 'Gill', 'Nemo', 'Bruce'];
+
+for (let i = 0; i < fish.length; i += 1) {
+  console.log(fish[i]);
+
+  if (fish[i] === 'Nemo') {
+    break;
+  }
+}
+
+Dory
+Marlin
+Gill
+Nemo
+```
+
+### Do...While
+
+```js
+// while
+let counter = 0;
+
+// condition checked before executing the block
+while (counter > 0) {
+  console.log('Woooot!');
+  counter -= 1;
+}
+
+
+// do...while
+let counter = 0;
+
+// condition checked after the block has executed
+// executed at least once, even if condition not satisfied
+do {
+  console.log('Woooot!');
+  counter -= 1;
+} while (counter > 0);
+// 'Woooot!'
+```
--- Courses/JS100/JavaScript-Basics/code.js
+++ Courses/JS100/JavaScript-Basics/code.js
@@ -2,9 +2,9 @@
 // console.log(word.charAt(0).toUpperCase() + word.substring(1));
 // => Mountain
 
-function capitalize(word) {
-  return word[0].toUpperCase() + word.slice(1).toLowerCase();
-}
+// function capitalize(word) {
+//   return word[0].toUpperCase() + word.slice(1).toLowerCase();
+// }
 // console.log(capitalize('mountain'));
 
 let dinner = ['fish', 'and', 'chips'];
@@ -50,8 +50,150 @@ let trees = ['birch', 'pine', 'sequoia', 'palm tree'];
 // TypeError: tweet.length is not a function
 
 // TypeError fixed
-let tweet = 'Woohoo! :-)';
+// let tweet = 'Woohoo! :-)';
 
-if (tweet.length > 140) {
-  console.log('Tweet is too long!');
-}
+// if (tweet.length > 140) {
+//   console.log('Tweet is too long!');
+// }
+
+// Countdown
+// for (let i = 10; i > 0; i -= 1) {
+//   console.log(i);
+// }
+
+// console.log('Launch!');
+
+// Alternative
+// for (let i = 10; i >= 0; i -= 1) {
+//   if (i === 0) {
+//     console.log('Launch!');
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// Triple Greeting
+
+// Write a loop that logs greeting three times.
+// let greeting = 'Aloha!';
+
+// let greeting = 'Aloha!';
+
+// for (let count = 0; count < 3; count += 1) {
+//   console.log(greeting);
+// }
+
+// let greeting = 'Aloha!';
+// let count = 1;
+
+// while (count <= 3) {
+//   console.log(greeting);
+//   count += 1;
+// }
+
+// Take Two
+// Write a for loop that iterates over all numbers from 1 to 100, and outputs the result of multiplying each element by 2.
+
+// for (let num = 1; num <= 100; num += 1) {
+//   console.log(num * 2);
+// }
+
+// Using the code below as a starting point, write a while loop that logs the elements of array at each index, and terminates after logging the last element of the array.
+
+// let array = [1, 2, 3, 4];
+// let index = 0;
+
+// while (index < array.length) {
+//   console.log(array[index])
+//   index += 1;
+// }
+
+// Continue
+// write a for loop that loops over the elements of the array cities and logs the length of each string to the console. If the element is null, skip forward to the next iteration without logging anything to the console.
+
+// let cities = [
+//   'Istanbul',
+//   'Los Angeles',
+//   'Tokyo',
+//   null,
+//   'Vienna',
+//   null,
+//   'London',
+//   'Beijing',
+//   null,
+// ];
+
+// for (let i = 0; i < cities.length; i += 1) {
+//   if (cities[i] === null) {
+//     continue
+//   }
+//   console.log(cities[i].length);
+// }
+
+// for (let i = 0; ; i += 1) {
+//   console.log('and on');
+// }
+
+// for (let i = 0; ; i += 1) {
+//   console.log('and on');
+//   break;
+// }
+
+// Write a while loop that logs all odd natural numbers between 1 and 40.
+
+// let num = 1;
+
+// method 1
+// while (num < 40) {
+//   console.log(num);
+//   num += 2;
+// }
+
+// method 2
+// let num = 1;
+
+// while (num <= 40) {
+//   if (num % 2 === 1) {
+//     console.log(num);
+//   }
+
+//   num += 1;
+// }
+
+// method 3
+// let num = 1;
+
+// while (num <= 40) {
+//   if (num % 2 !== 0) {
+//     console.log(num);
+//   }
+
+//   num += 1;
+// }
+
+// Loop over the elements of the array fish, logging each one. Terminate the loop immediately after logging the string 'Nemo'.
+
+// let fish = ['Dory', 'Marlin', 'Gill', 'Nemo', 'Bruce'];
+
+// for (let i = 0; i < fish.length; i += 1) {
+//   console.log(fish[i]);
+
+//   if (fish[i] === 'Nemo') {
+//     break;
+//   }
+// }
+
+// let counter = 0;
+
+// while (counter > 0) {
+//   console.log('Woooot!');
+//   counter -= 1;
+// }
+
+let counter = 0;
+
+do {
+  console.log('Woooot!');
+  counter -= 1;
+} while (counter > 0); 
+// Woooot!
