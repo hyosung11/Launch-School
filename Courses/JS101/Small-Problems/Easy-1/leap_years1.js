e35b0b3bbd99a3b01863cc5d201555d857b56ed2
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-16 13:16:37 -0400

    Write `leap_years1.js` discussion and further exploration notes

--- Courses/JS101/Small-Problems/Easy-1/leap_years1.js
+++ Courses/JS101/Small-Problems/Easy-1/leap_years1.js
@@ -65,14 +65,19 @@ CODE
 Implementation of Algorithm
 */
 
+// function isLeapYear(year) {
+//   if (year % 400 === 0) {
+//     return true;
+//   } else if (year % 100 === 0) {
+//     return false;
+//   } else {
+//     return year % 4 === 0;
+//   }
+// }
+
+// LS shorter solution
 function isLeapYear(year) {
-  if (year % 400 === 0) {
-    return true;
-  } else if (year % 100 === 0) {
-    return false;
-  } else {
-    return year % 4 === 0;
-  }
+  return (year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0);
 }
 
 // console.log(isLeapYear(2016));      // true
@@ -81,10 +86,34 @@ function isLeapYear(year) {
 // console.log(isLeapYear(2400));      // true
 // console.log(isLeapYear(240000));    // true
 // console.log(isLeapYear(240001));    // false
-// console.log(isLeapYear(2000));      // true
+console.log(isLeapYear(2000));      // true
 // console.log(isLeapYear(1900));      // false
 // console.log(isLeapYear(1752));      // true
 // console.log(isLeapYear(1700));      // false
-// console.log(isLeapYear(1));         // false
+console.log(isLeapYear(1));         // false
 console.log(isLeapYear(100));       // false
 console.log(isLeapYear(400));       // true
+
+/* 
+Discussion
+
+The first solution takes this one step at a time by testing for the least common cases first: years divisible by 400, and years divisible by 100. If the year is anything else, then it is a leap year if it is divisible by 4.
+
+The second solution is more idiomatic, but also a little harder to read. It is, in effect, identical to the first solution, with the tests turned around a little bit.
+
+Further Exploration
+
+The order in which you perform tests for a leap year calculation is important. For what years will `isLeapYear` fail if you rewrite it as shown below?
+
+function isLeapYear(year) {
+  if (year % 100 === 0) {
+    return false;
+  } else if (year % 400 === 0) {
+    return true;
+  } else {
+    return year % 4 === 0;
+  }
+}
+
+Can you rewrite `isLeapYear` to perform its tests in the opposite order of the above solution? That is, test whether the year is divisible by 4 first, then, if necessary, test whether it is divisible by 100, and finally, if necessary, test whether it is divisible by 400. Is this solution simpler or more complex than the original solution?
+*/
