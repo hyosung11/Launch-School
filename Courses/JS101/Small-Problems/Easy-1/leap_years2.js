199bc3b72d794ab3b54ec36ce7b41187cab061d8
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-16 16:20:24 -0400

    Submit `multiples_of_3_and_5.js` solution (worked with Laurent)

---
+++ Courses/JS101/Small-Problems/Easy-1/leap_years2.js
@@ -0,0 +1,48 @@
+/* JS101 - Small Problems > Easy 1 > 9. Leap Years (Part 2)
+
+Leap Years (Part 2)
+
+This is a continuation of the previous exercise.
+
+The British Empire adopted the Gregorian Calendar in 1752, which was a leap year. Prior to 1752, they used the Julian Calendar. Under the Julian Calendar, leap years occur in any year that is evenly divisible by 4.
+
+Using this information, update the function from the previous exercise to determine leap years both before and after 1752.
+
+PEDAC: process the problem (PEDA) and code with intent (C)
+
+PROBLEM
+Identify expected input and output
+Make the requirements explicit
+Identify rules
+Mental model of the problem (optional)
+
+EXAMPLES / TEST CASES
+Validate understanding of the problem
+
+Examples:
+isLeapYear(2016);      // true
+isLeapYear(2015);      // false
+isLeapYear(2100);      // false
+isLeapYear(2400);      // true
+isLeapYear(240000);    // true
+isLeapYear(240001);    // false
+isLeapYear(2000);      // true
+isLeapYear(1900);      // false
+isLeapYear(1752);      // true
+isLeapYear(1700);      // true
+isLeapYear(1);         // false
+isLeapYear(100);       // true
+isLeapYear(400);       // true
+
+Edge Cases?
+
+DATA STRUCTURE
+How we represent the data that we will work with when converting the input to output
+
+ALGORITHM
+steps for converting input to output
+
+CODE
+Implementation of Algorithm
+
+*/
