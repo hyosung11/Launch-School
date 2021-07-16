559258ffccc128cec8b400ebac6ed3b6c7563ea4
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-16 10:46:37 -0400

    Submit `short_long_short.js` solution

--- Courses/JS101/Small-Problems/Easy-1/code_snippets.js
+++ Courses/JS101/Small-Problems/Easy-1/code_snippets.js
@@ -1,3 +1,4 @@
+// use brackets for flow control
 // if (x === 3);
 //   console.log('x is 3');
 //   console.log('x is an odd number');
---
+++ Courses/JS101/Small-Problems/Easy-1/short_long_short.js
@@ -0,0 +1,56 @@
+/* JS101 - Small Problems > Easy 1 > 7. Short Long Short
+
+Short Long Short
+
+Write a function that takes two strings as arguments, determines the length of the two strings, and then returns the result of concatenating the shorter string, the longer string, and the shorter string once again. You may assume that the strings are of different lengths.
+
+PROBLEM
+Identify expected input and output
+- input: two strings
+- output: concatenated string
+
+Make the requirements explicit
+- input two string arguments
+- determine the length of the two strings
+- concatenate shorter string + longer string + shorter string
+- return concatenated string
+
+Identify rules (problem domain; implicit concepts)
+- assume strings are different lengths
+- remember to test code along the way
+
+EXAMPLES / TEST CASES
+- Validate understanding of the problem
+- Examples:
+  shortLongShort('abc', 'defgh');    // "abcdefghabc"
+  shortLongShort('abcde', 'fgh');    // "fghabcdefgh"
+  shortLongShort('', 'xyz');         // "xyz"
+- Edge Cases?
+
+DATA STRUCTURE
+How we represent data that we will work with when converting the input to output.
+- none
+
+ALGORITHM
+Steps for converting input to output
+1. declare function with two parameters that take string arguments
+2. compare the lengths of the two strings
+3. concatenate the strings with shorter + longer + shorter
+4. return concatenated string
+
+CODE
+Implementation of Algorithm
+*/
+
+// declare function
+function shortLongShort(string1, string2) {
+  if (string1.length > string2.length) {
+    return string2 + string1 + string2;
+  } else {
+    return string1 + string2 + string1;
+  }
+}
+
+console.log(shortLongShort('abc', 'defgh')); // "abcdefghabc"
+console.log(shortLongShort('abcde', 'fgh')); // "fghabcdefgh"
+console.log(shortLongShort('', 'xyz')); // "xyz"
