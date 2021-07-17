ace5612d7aef9e5ea445a0b18f2319bf94164e2b
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-17 10:25:34 -0400

    Submit `utf-16_string_value.js` solution

---
+++ Courses/JS101/Small-Problems/Easy-1/welcome_stranger.js
@@ -0,0 +1,44 @@
+/* JS101 - Small Problems > Easy 2 > 1. Welcome Stranger
+
+Welcome Stranger
+
+Create a function that takes 2 arguments, an array and an object. The array 
+will contain 2 or more elements that, when combined with adjoining spaces, 
+will produce a person's name. The object will contain two keys, "title" and 
+"occupation", and the appropriate values. Your function should return a 
+greeting that uses the person's full name, and mentions the person's title.
+
+PEDAC => process the problem (PEDA) and code with intent (C)
+
+PROBLEM
+Identify expected input and output
+- input: 
+- output:
+
+Problem Domain (including implicit requirements)
+
+Make the requirements explicit (clarifying questions)
+
+Identify rules
+
+Mental model of the problem (optional)
+
+EXAMPLES / TEST CASES
+Validate understanding of the problem
+
+Examples:
+console.log(
+  greetings(["John", "Q", "Doe"], { title: "Master", occupation: "Plumber" })
+);
+// logs Hello, John Q Doe! Nice to have a Master Plumber around.
+
+Edge Cases?
+
+DATA STRUCTURE
+How we represent the data that we will work with when converting the input to output.
+
+ALGORITHM
+Steps for converting input to output
+
+CODE
+Implementation of Algorithm
