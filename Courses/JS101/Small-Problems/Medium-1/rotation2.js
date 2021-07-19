ce95f388b498e022ca515a45e47814309576c9ae
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-19 06:14:03 -0400

    Setup `rotation2.js`

---
+++ Courses/JS101/Small-Problems/Medium-1/rotation2.js
@@ -0,0 +1,51 @@
+/* JS101 - Small Problems > Medium 1 > 2. Rotation (Part 2)
+
+Rotation (Part 2)
+
+Write a function that rotates the last count digits of a number. To perform the rotation, move the first of the digits that you want to rotate to the end and shift the remaining digits to the left.
+
+PEDAC => process the problem (PEDA) and code with intent (C)
+
+PROBLEM
+Identify expected input and output
+
+- input: two numbers
+- output: number
+
+Problem Domain (including implicit requirements)
+
+Make the requirements explicit (clarifying questions)
+
+Identify rules
+
+Mental model of the problem (optional)
+
+EXAMPLES / TEST CASES
+Validate understanding of the problem
+
+Examples:
+rotateRightmostDigits(735291, 1);      // 735291
+rotateRightmostDigits(735291, 2);      // 735219
+rotateRightmostDigits(735291, 3);      // 735912
+rotateRightmostDigits(735291, 4);      // 732915
+rotateRightmostDigits(735291, 5);      // 752913
+rotateRightmostDigits(735291, 6);      // 352917
+
+Edge Cases?
+
+DATA STRUCTURE
+How we represent the data that we will work with when converting the input to output.
+
+- input
+- intermediary
+- output
+
+ALGORITHM
+Steps for converting input to output
+
+CODE
+Implementation of Algorithm
+
+- test code while programming
+
+*/
