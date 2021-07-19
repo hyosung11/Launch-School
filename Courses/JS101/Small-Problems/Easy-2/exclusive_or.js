71a45ea724c61e794564b53ce229b174356ee006
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-19 19:31:34 -0400

    Submit  solution with LS sollution and discussion notes

--- Courses/JS101/Small-Problems/Easy-2/exclusive_or.js
+++ Courses/JS101/Small-Problems/Easy-2/exclusive_or.js
@@ -65,17 +65,58 @@ Implementation of Algorithm
 
 */
 
-function xor(operand1, operand2) {
-  if (Boolean(operand1) === false && Boolean(operand2) === true) {
+// function xor(value1, value2) {
+//   if (Boolean(value1) === true && Boolean(value2) === false) {
+//     return true;
+//   } else if (Boolean(value1) === false && Boolean(value2) === true) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// LS solution is great!
+function xor(value1, value2) {
+  if ((value1 && !value2) || (value2 && !value1)) {
     return true;
-  } else if (Boolean(operand1) === true && Boolean(operand2) === false) {
-    return true;
-  } else {
-    return false;
   }
+  return false;
+}
+
+// LS shorter version
+function xor(value1, value2) {
+  return!!((value1 && value2) || (value2 && !value1));
 }
 
 console.log(xor(5, 0)); // true
 console.log(xor(false, true)); // true
 console.log(xor(1, 1)); // false
 console.log(xor(true, true)); // false
+console.log(xor(null, true)); // true
+console.log(xor(NaN, true)); // true
+
+/*
+Discussion
+This implementation is straight forward; it simply returns `true` if exactly one of the values is truthy; otherwise, it returns `false`.
+*/
+
+function xor(value1, value2) {
+  return !!((value1 && !value2) || (value2 && !value1));
+}
+
+/* This is more idiomatic than the first solution, but it does take a little more effort to figure out what is going on. Note that this solution requires `!!` to force the return value to a boolean.
+
+Some of you may be tempted to use the `^` operator for this exercise. The ^ operator is a bit-wise operator for performing exclusive-or bit operations. In some cases, you can use `^` as an exclusive-or operator. However, it only works properly when both values are numeric or both are boolean -- anything else may lead to unexpected results. For instance, you might expect the following expression to return a truthy value:
+*/
+
+'a' ^ false
+
+/*
+It doesn't. Instead, it returns a falsy value (`0`).
+
+Further Exploration
+
+Can you think of a situation in which a boolean `xor` function would be useful? Suppose you were modeling a light at the top of a flight of stairs wired in such a way that the light can be turned on or off using either the switch at the bottom of the stairs or the switch at the top of the stairs. This is an xor configuration, and it can be modeled in JavaScript using the xor function. Think of some additional examples.
+
+`||` and `&&` are so-called short circuit operators in that the second operand is not evaluated if its value is not needed. Does the `xor` function perform short-circuit evaluation of its operands? Why or why not? Does short-circuit evaluation in `xor` operations even make sense?
+*/
