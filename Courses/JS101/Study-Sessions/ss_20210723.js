3ea86fed09e2538389c142ddf61f8047a28ec9e1
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-23 12:15:48 -0400

    Add notes and code from study session

---
+++ Courses/JS101/Study-Sessions/ss_20210723.js
@@ -0,0 +1,128 @@
+/* JS100/JS101 Beginner Study Group | Friday, July, 23rd | 11:00 AM Eastern / 8:00 AM PACIFIC
+
+PARTICIPANTS
+Mandy Cheang TA
+Jeannine (JS101)
+  - Boston, MA
+  - Lesson 4
+@GregB
+  - Denver, CO
+  - JS101 Lesson 4, Easy 4
+@AlexStein
+  - NJ music teacher
+  - two year plan for LS
+@laurentstaub
+  - Paris France
+  - Lesson 2
+  - Consultant of own company on Excel
+@HyoSungBidol-Lee
+
+AGENDA
+- Introductions
+- Study Tips - Learning how to learn
+  - cards at different levels
+  - click through to the summary
+- Statement vs Expression
+- Problem solving - PEDAC
+
+STATEMENT vs EXPRESSION
+
+Statement
+- does not evaluate to a value
+  - console.log(if(true))
+
+Expression
+- an expression is anything that JS can evaluate to a value, even if that value is undefined or null
+- 7 + (5 + 2)
+// 14
+
+=====
+PEDAC
+
+Given a string of one or more words, return an array that contains
+the number of vowels in each word of the argument string.
+
+The returned array should have the same number of
+elements as words in the argument string.
+
+PROBLEM
+Input
+- string of one or more words
+- What is word? -- group of characters separated by a white space
+
+Output:
+- an array, elements are numbers. Numbers represent the count of vowels of each word.
+- vowels: 'aeiouAEIOU'
+
+Explicit Requirements
+- returned array should have the same number of elements as words
+
+EXAMPLES
+- `!!` is considered a word in the 2nd example
+- words can be a combination of letters and symbols 'abc' 'grrr!'
+- when given an empty string that contains no words, return an empty array
+- can assume that words do not contain numbers
+
+Edge Cases
+- inclusion of numbers?
+  - can ask in an interview
+
+DATA STRUCTURES
+- input String
+- output array of numbers
+- another data structure convert String --> Array
+
+ALGORITHM
+1. declare function with one parameter that takes a string argument of one or more words
+2. initialize an empty array to log the number of vowels in each word
+3. change the string to all lowercase
+4. split the string at each word
+5. initialize variable to count vowels of each word
+6. loop through check each word for a vowel
+  - if vowel increase the count of vowels for the word
+  - check next word to end of string
+7. return array of numbers indicating how many vowels in each word
+
+Greg's Algorithm
+
+Mandy
+- Convert the input string of words into an array of words.
+    - Split the string by spaces to extract each word into the array. 
+- Convert the array of words into a new array of numbers.
+- Iterate over each word in the array of words and count the number of vowels for each word, using the `numOfVowels(word)` helper function. 
+
+    - Create a helper function for the sub-problem of counting  the number of vowels in a word. `numOfVowels` function:
+        - Given a single word.
+        - Initialize a variable `count` to store the count.
+        - Iterate over each character of the word.
+        - If the character is one of `a` `e` `i` `o` `u` , - increment `count`. 
+        - After iteration is complete, return `count`.
+
+- Return the newly created array of numbers (number of vowels).
+*/
+
+function vowelCount(str) {
+  if (str === '') return [];
+
+  return str.split(' ').map((word) => countVowels(word));
+}
+
+function countVowels(word) {
+  let count = 0;
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  for (let character of word) {
+    if (vowels.includes(character.toLowerCase())) {
+      count += 1;
+    }
+  }
+
+  return count;
+}
+
+// Examples
+vowelCount('WhaTs yOur enneagram?');                  // [1, 2, 4]
+vowelCount('Colonel Sanders feeds me well !!');       // [3, 2, 2, 1, 1, 0]
+vowelCount('');                                       // []
+vowelCount('ZoInkies!! There are monsters in here.'); // [4, 2, 2, 2, 1, 2]
+vowelCount('grrr!');                                  // [0]
---
+++ Courses/JS101/Study-Sessions/ss_intro_pedac.js
@@ -0,0 +1,168 @@
+/* JS100/101 Study Session: Intro to PEDAC | Wed. July 14th | 12:00 PM Eastern / 09:00 AM Pacific
+
+Consider the word "abode".
+The letter `a` is in position 1 and `b` is in position 2.
+In the alphabet, `a` and `b` are also in positions 1 and 2.
+
+The letters `d` and `e` in "abode" occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
+
+Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word. For example,
+
+solve(["abode","ABc","xyzD"]) // [4, 3, 1]
+
+Input will consist of alphabetic characters, both uppercase and lowercase. No spaces.
+
+PEDAC
+
+Problem
+
+inputs:
+- array of strings
+
+outputs:
+- array of numbers (with the same number of elements as the input array)
+
+rules:
+- a letter corresponds to a number
+- to each string corresponds a SamePositionCount
+- case doesn't matter
+- no spaces in the strings
+- no special characters => only alphabet letters
+
+what's a same position count?
+- for each letter in a word, we need to compare the letter's position in the word and in the alphabet. If they match, the "return count" increases by 1.
+
+Examples
+
+rules:
+- alphabet position: starts at 1 => can we make it start at 0?
+- word position: starts at 0
+
+Data Structure
+
+input: array of strings
+output: array of integers
+
+intermediary steps: array
+
+Array.prototype.map()
+
+
+Algorithm
+
+Given a collections of strings
+
+Set a var alphabet to the lowercase alphabet
+Set a var resultArray to an empty array
+
+loop through the collection of strings one by one
+  - set a counter to 0
+  -for each string in collection, loop through the string
+    we check the character in the string at the current index compared with
+    the character in alphabet at the current index
+    if the chars match, increment counter
+  after we iterate through a single string, push counter to result array
+  -after we iterate through the collection of strings
+  - return result array
+
+Byron's Algorithm
+
+Given a collections of strings
+
+Set a var alphabet to the lowercase alphabet
+Set a var resultArray to an empty array
+
+loop through the collection of strings one by one
+
+  - set a counter to 0
+  -for each string in collection, we loop through the string
+    we check the character in the string at the current index compared with
+    the character in alphabet at the current index
+    if the chars match, increment counter
+  after we iterate through a single string, push counter to result array
+  - after we iterate through the collection of strings
+return result array
+*/
+
+function countMatchingIndices(array) {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let resultArray = [];
+
+  // loop through the array
+  for (let indexOfArray = 0; indexOfArray < array.length; indexOfArray += 1) {
+    let count = 0;
+    let word = array[indexOfArray];
+
+    // loop through string
+    for (let indexOfString = 0; indexOfString < word.length; indexOfString += 1) {
+      if (word[indexOfString].toLowerCase() === alphabet[indexOfString]) {
+        count += 1;
+      }
+    }
+
+    resultArray.push(count);
+  }
+
+  return resultArray;
+}
+
+
+// function countMatchingIndices(array) {
+//   let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+//   let resultArray = [];
+
+//   for (let index = 0; index < array.length; index++) {
+//     let count = 0;
+//     let word = array[index];
+
+//     for (let stringIndex = 0; stringIndex < word.length; stringIndex++) {
+//       if (word[stringIndex].toLowerCase() === alphabet[stringIndex]) {
+//         count += 1;
+//       }
+//     }
+
+//     resultArray.push(count);
+//   }
+
+//   return resultArray;
+// }
+
+// countMatchingIndices()
+// Version 1
+
+// function solve(array) {
+//   let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+//   let resultArray = [];
+
+//   for (let index = 0; index < array.length; index++) {
+//     let count = 0;
+//     let word = array[index];
+//     for (let stringIndex = 0; stringIndex < word.length; stringIndex++) {
+//       if (word[stringIndex].toLowerCase() === alphabet[stringIndex]) {
+//         count += 1;
+//       }
+//     }
+//     resultArray.push(count);
+//   }
+//   return resultArray;
+// }
+
+console.log(countMatchingIndices(['abode', 'ABc', 'xyzD'])); // [4, 3, 1]
+console.log(countMatchingIndices(['abide', 'ABc', 'xyz'])); // [4, 3, 0]
+console.log(countMatchingIndices(['IAMDEFANDJKL', 'thedefgh', 'xyzDEFghijabc'])); // [6, 5, 7]
+console.log(countMatchingIndices(['encode', 'abc', 'xyzD', 'ABmD'])); // [1, 3, 1, 3]
+console.log(countMatchingIndices([])); // []
+
+
+// Version 2 with filer and map
+// function countMatchingIndices(str) {
+//   let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+//   return str.split('').filter((char, idx) => {
+//     return alphabet.indexOf(char.toLowerCase()) === idx;
+//   }).length;
+// }
+
+// function solve(arr) {
+//   return arr.map((str) => countMatchingIndices(str));
+// }
