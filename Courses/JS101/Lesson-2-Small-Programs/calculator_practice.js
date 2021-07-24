173e68878f383c733574f9c6abe7073e887eb190
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-24 12:39:28 -0400

    Add config file to calculator

--- Courses/JS101/Lesson-2-Small-Programs/calculator_practice.js
+++ Courses/JS101/Lesson-2-Small-Programs/calculator_practice.js
@@ -4,7 +4,34 @@ ask the user for the second number
 perform the operation
 display the result of the operation
 
+Extract messages in the program to a configuration file.
+
+There are several messages sprinkled throughout the program. Can we move them into some configuration file and access them by key? That would let us manage the messages more easily, and we could even internationalize the messages.
+
+Hints
+- Use the JSON format to store messages in a file called `calculator_practice_messages.json`
+- Use `require('./calculator_practice_messages.json`) to load the file into your program as an object.
+- Access the messages from the object with object property access syntax.
+
+Solution
+
+First, we need to extract the messages into a configuration file. We can use any format, from plain text files to YAML to CSV. Npm has libraries that can help with parsing those formats, but we're not yet ready to tackle npm libraries. Most JavaScript developers prefer the JSON format, so we'll use that. We'll extract our messages into a file named `calculator_practice_messages.json. Make sure this file is in the same directory as your calculator program.
+
+As you can see, our JSON configuration is just a list of key/value pairs like a JavaScript object. We only show two key-value pairs, but you can add more. Just be sure to follow the syntax shown.
+
+calculator_practice_messages.json
+
+{
+  "welcome": "Welcome to Calculator!",
+  "firstNumber": "What is the first number?"
+}
+
+load `calculator_practice_messages` into the program at the top of the file
+This line will load the contents of the calculator_messages.json file in the form of an object and assign it to the MESSAGES constant. Since it's an ordinary object, we don't have to do anything before we use it. We can just start accessing its properties as needed:
+
 */
+const MESSAGES = require('./calculator_practice_messages.json');
+
 const readline = require('readline-sync');
 
 function prompt(message) {
@@ -15,32 +42,34 @@ function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
-prompt("Welcome to the Calculator!");
+// replacec this:
+// prompt("Welcome to the Calculator!");
+
+// with this:
+prompt(MESSAGES['welcome']);
 
 while (true) {
-  prompt('What is the first number?');
+  prompt(MESSAGES['number1']);
   let number1 = readline.question();
 
   while (invalidNumber(number1)) {
-    prompt("Not a valid number. Please try again.");
+    prompt(MESSAGES['invalidNumber']);
     number1 = readline.question();
   }
 
-  prompt('What is the second number?');
+  prompt(MESSAGES['number2']);
   let number2 = readline.question();
 
   while (invalidNumber(number2)) {
-    prompt("Not a valid number. Please try again.");
+    prompt(MESSAGES['invalidNumber']);
     number2 = readline.question();
   }
 
-  prompt(
-    'What operation do you want to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide'
-  );
+  prompt(MESSAGES['operation']);
   let operation = readline.question();
 
   while (!['1', '2', '3', '4'].includes(operation)) {
-    prompt('Please choose 1, 2, 3 or 4.');
+    prompt(MESSAGES['operationChoice']);
     operation = readline.question();
   }
 
@@ -63,8 +92,10 @@ while (true) {
 
   prompt(`The result is ${output}.`);
 
-  prompt("Do you want to perform another calculation? (y/n");
+  prompt(MESSAGES['anotherCalculation']);
   let answer = readline.question();
 
-  if (answer[0].toLowerCase() !== 'y') break;
+  if (answer[0].toLowerCase() !== 'y')
+    prompt(MESSAGES['thanks']);
+    break;
 }
