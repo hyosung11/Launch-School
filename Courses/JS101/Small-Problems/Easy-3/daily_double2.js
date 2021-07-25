a61db5b4de768eb5e9c5e4b3197f5dd4104dcf81
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-25 07:32:39 -0400

    Commit files from study session with Alex

---
+++ Courses/JS101/Small-Problems/Easy-3/daily_double2.js
@@ -0,0 +1,75 @@
+/* ddaaiillyy ddoouubbllee
+
+Write a function that takes a string argument and returns a new string that contains the value of the original string with all consecutive duplicate characters collapsed into a single character.
+
+PEDAC => process the problem (PEDA) and code with intent (C)
+
+PROBLEM
+input - string
+output - string
+
+Make the requirements explicit (clarifying questions)
+- no duplicate characters
+
+Identify rules
+- if char doesnt equal the char after it, add it to the new string
+- if string is empty return an empty string
+
+EXAMPLES / TEST CASES
+Validate understanding of the problem
+
+Examples:
+
+crunch('ddaaiillyy ddoouubbllee');    // "daily double"
+crunch('4444abcabccba');              // "4abcabcba"
+crunch('ggggggggggggggg');            // "g"
+crunch('a');                          // "a"
+crunch('');                           // ""
+
+Edge Cases?
+- none
+
+DATA STRUCTURE
+How we represent the data that we will work with when converting the input to output.
+
+- input: string
+- intermediary
+- output: string
+
+ALGORITHM
+Steps for converting input to output
+1. declare a function with one parameter that takes a string argument
+2. check if string is empty / string length is zero
+  - if empty return an empty string
+3. initialize variable for the string to return as an empty string
+4. loop through the string
+  - if char doesnt equal the char after it, add it to the new string
+  - go to the next character
+5. return new string
+
+CODE
+Implementation of Algorithm
+
+- test code while programming
+
+*/
+function crunch(string) {
+  if (string.length === 0) return '';
+
+  let result = '';
+
+  for (let index = 0; index < string.length; index++) {
+    if (string[index] !== string[index + 1]) {
+      result += string[index];
+    }
+  }
+
+  return result;
+}
+
+// Examples:
+console.log(crunch('ddaaiillyy ddoouubbllee'));    // "daily double"
+console.log(crunch('4444abcabccba'));              // "4abcabcba"
+console.log(crunch('ggggggggggggggg'));            // "g"
+console.log(crunch('a'));                          // "a"
+console.log(crunch(''));                           // ""
---
+++ Courses/JS101/Small-Problems/Easy-3/stringy_strings.js
@@ -0,0 +1,74 @@
+/* JS101 - Small Problems > Easy 3 > 3. Stringy Strings
+
+Stringy Strings
+
+Write a function that takes one argument, a positive integer, and returns a string of alternating '1's and '0's, always starting with a '1'. The length of the string should match the given integer.
+
+# PEDAC => process the problem (PEDA) and code with intent (C)
+
+PROBLEM
+Identify expected input and output
+
+- input: number
+- output: string
+
+Problem Domain (including implicit requirements)
+
+Make the requirements explicit (clarifying questions)
+
+Identify rules
+-return of length number that alternates between 1 and 0 for the entire length of the string, starting with 1
+
+Mental model of the problem (optional)
+
+EXAMPLES / TEST CASES
+Validate understanding of the problem
+
+Examples:
+stringy(6);    // "101010"
+stringy(9);    // "101010101"
+stringy(4);    // "1010"
+stringy(7);    // "1010101"
+
+Edge Cases?
+
+DATA STRUCTURE
+How we represent the data that we will work with when converting the input to output.
+
+- input: string
+- intermediary
+- output: string
+
+ALGORITHM
+Steps for converting input to output
+1. declare a function with one parameter that takes a string argument
+2. initialize variable string for an empty string
+3. loop number times
+  - if index % 2 == 0, string += '1'
+  - else add 0 to string
+4. return numeric string
+
+CODE
+Implementation of Algorithm
+
+- test code while programming
+
+*/
+function stringy(value) {
+  let result = '';
+
+  for (let index = 0; index < value; index += 1) {
+    if (index % 2 === 0) {
+      result += '1';
+    } else {
+      result += '0';
+    }
+  }
+
+  return result;
+}
+
+console.log(stringy(9));    // "101010101"
+console.log(stringy(4));    // "1010"
+console.log(stringy(6));    // "101010"
+console.log(stringy(7));    // "1010101"
