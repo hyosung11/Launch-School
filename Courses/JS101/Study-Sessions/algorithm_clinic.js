dc2961bc4e28b8b0793472f1fb89bd7a65382ad7
HyoSung Bidol-Lee <hyosung11@me.com>
2021-07-27 13:03:10 -0400

    Commit algorithm clinic notes and code

---
+++ Courses/JS101/Study-Sessions/algorithm_clinic.js
@@ -0,0 +1,160 @@
+/* Use the PEDAC process to get a start on solving the problem below...
+# ... but STOP when you finish writing your algorithm.
+# Then let your partner know you're done.
+# Once each of you finish, switch "computers" by moving your avatar across the table
+# Copy and paste your partner’s algorithm to your own repl or code editor
+# Code up a solution to the problem that your partner was given
+# If you have any trouble interpreting their algorithm, ask them to adjust it
+# Good luck!!
+
+# Given a string and array of subarrays that could contain string elements, return an integer representing the number of times an anagram of the given string occurs in the collection. Two words are anagrams if they contain the same letters in the same quantities; in other words, to create an anagram from a word, you must scramble the word’s letters but not add or delete any letters. Note that two words are not considered anagrams if they are the same word.
+
+# Javascript test cases:
+console.log(anagramCounter('reap', [['scram', 'beat', 45, null, 'pear'], [true, 'reap']]) == 1);
+console.log(anagramCounter('meat', [['meat', 'mate', 'meaty', 'matey'], ['tame', 'team']]) == 3);
+console.log(anagramCounter('VEIL', [['VILE', 'vile', 'evil'], []]) == 3);
+console.log(anagramCounter('veil', [['VILE', 'evil'], []]) == 2);
+
+# Ruby test cases:
+p anagram_counter('reap', [['scram', 'beat', 45, nil, 'pear'], [true, 'reap']]) == 1
+p anagram_counter('meat', [['meat', 'mate', 'meaty', 'matey'], ['tame', 'team']]) == 3
+p anagram_counter('VEIL', [['VILE', 'vile', 'evil'], []]) == 3
+p anagram_counter('veil', [['VILE', 'evil'], []]) == 2
+
+==============================================================
+
+# PEDAC => process the problem (PEDA) and code with intent (C)
+
+PROBLEM
+Identify expected input and output
+
+- input: string and array of subarrays
+- output: integer
+
+Problem Domain (including implicit requirements)
+
+Make the requirements explicit (clarifying questions)
+- return an integer representing the number of times an anagram of the given string occurs in the collection
+
+Identify rules
+- anagram if the contain the same letters in the same quantities
+- scramble word's letters but not add or delete any letters
+- two words that are the same word are not anagrams
+
+Mental model of the problem (optional)
+
+EXAMPLES / TEST CASES
+Validate understanding of the problem
+
+Examples:
+# Javascript test cases:
+console.log(anagramCounter('reap', [['scram', 'beat', 45, null, 'pear'], [true, 'reap']]) == 1);
+console.log(anagramCounter('meat', [['meat', 'mate', 'meaty', 'matey'], ['tame', 'team']]) == 3);
+console.log(anagramCounter('VEIL', [['VILE', 'vile', 'evil'], []]) == 3);
+console.log(anagramCounter('veil', [['VILE', 'evil'], []]) == 2);
+
+Edge Cases?
+
+DATA STRUCTURE
+How we represent the data that we will work with when converting the input to output.
+
+- input: string, array with subarrays
+- intermediary: string
+- output: integer
+
+ALGORITHM
+1. Declare a function with two parameters, a string and an array with subarrays.
+2. Initialize variable to capture the string argument
+3. Initialize variable to count the number of anagrams in the array
+4. Loop through the array to see if any combination of letters matches the string to make an anagram
+  - An anagram contains the same letters in the same quantities as the string
+  - anagram has same letters but no added or deleted letters
+  - two words that are the same words are not anagrams
+5. If any elements in the array match the string, increase the count of anagrams.
+6. Return the count as an integer representing the number of anagrams in the array.
+
+Revised by Stan
+ALGORITHM
+1. Declare a function with two parameters, a string and an array with subarrays.
+2. Initialize variable to capture the string argument
+3. Initialize variable to count the number of anagrams in the array
+4. Loop through the elements of the flattened array
+For each element (word) of the subarray that is an anagram, increase the count by 1.
+  - Return false if word is same as string
+  - Return true if sorted word is same as sorted string
+    - Sort alphabetically
+  - Else return false
+5. Return the count as an integer representing the number of anagrams in the array.
+
+
+CODE
+Implementation of Algorithm
+
+- test code while programming
+
+console.log(
+  anagramCounter('reap', [
+    ['scram', 'beat', 45, null, 'pear'],
+    [true, 'reap'],
+  ]) == 1
+);
+console.log(
+  anagramCounter('meat', [
+    ['meat', 'mate', 'meaty', 'matey'],
+    ['tame', 'team'],
+  ]) == 3
+);
+console.log(anagramCounter('VEIL', [['VILE', 'vile', 'evil'], []]) == 3);
+console.log(anagramCounter('veil', [['VILE', 'evil'], []]) == 2);
+*/
+
+/* Stan's Exercise
+
+# Use the PEDAC process to get a start on solving the problem below...
+# ... but STOP when you finish writing your algorithm.
+# Then let your partner know you're done.
+# Once each of you finish, switch "computers" by moving your avatar across the table
+# Copy and paste your partner’s algorithm to your own repl or code editor
+# Code up a solution to the problem that your partner was given
+# If you have any trouble interpreting their algorithm, ask them to adjust it
+# Good luck!!
+
+# Given an array of strings in which each string may or may not contain punctuation, return a copy of the array in which the alphabetical characters are in reverse order, but the punctuation stays in the same place. 
+
+# Javascript test cases:
+console.log(preservePunctuation(["shan't", "won't", "y'all'rn't", "fixin'"])) // ["tnah's", "tno'w", "t'nrl'la'y", "nixif'"];
+console.log(preservePunctuation(["'eard", "why??", "peter", "rabbit", ''])); // ["'drae", "yhw??", "retep", "tibbar", ''];
+
+# Ruby test cases:
+p preserve_punctuation(["shan't", "won't", "y'all'rn't", "fixin'"]) == ["tnah's", "tno'w", "t'nrl'la'y", "nixif'"]
+p preserve_punctuation(["'eard", "why??", "peter", "rabbit", '']) == ["'drae", "yhw??", "retep", "tibbar", '']
+
+### Algo
+
+loop through given array and use return value as transformation of each element
+  for each word
+    Create an array of the characters in word
+    find indices of all letters
+      iterate through characters and add index to indices array if it is a letter
+    collect all letters belonging to those indices (collected_letters)
+      iterate through indices array and use the character at that index as return value
+    reverse the collected letters
+    loop through each character of the word
+      if it is a letter, reassign this element to first letter in the collected_letters collection
+        and remove that letter from the collection
+    end
+    join the array with the reassigned elements to a string
+  end
+end
+*/
+
+function preservePunctuation(array) {
+  let reversedArray = [];
+
+
+}
+
+
+// Javascript test cases:
+console.log(preservePunctuation(["shan't", "won't", "y'all'rn't", "fixin'"])) // ["tnah's", "tno'w", "t'nrl'la'y", "nixif'"];
+console.log(preservePunctuation(["'eard", "why??", "peter", "rabbit", ''])); // ["'drae", "yhw??", "retep", "tibbar", ''];
---
+++ Courses/JS101/Study-Sessions/algorithm_clinic.md
@@ -0,0 +1,78 @@
+# Algorithm Clinic
+
+### Welcome! Take a moment to read the instructions and tips below. Have fun, and feel free to ask Katarina or Missy questions at any time during the event.
+
+We will be using the video chat platform [gather.town](http://gather.town) to host this event. 
+
+## Instructions
+
+1. When the event starts, you will be assigned a partner with which to swap algorithms.
+2. The two of you should go to either of the two breakout rooms in Gather Town where you will see tables and chairs.
+3. Choose a table, and move your avatar to the chair at one end of the table while your partner does the same.
+4. You and your partner will only be able to hear each other so you won't have to worry about disturbing others.
+5. Press 'x' in front of the "computer" at your side of the table to open up a memo pad with a link.
+6. The link will take you to a google doc where your problem will be pasted
+7. Start your PEDAC process, but 🛑 **stop** 🛑 when you finish your algorithm (don't code up the solution!) 
+8. Once you and your partner are finished with your algorithms, move your avatars to each other's computers to access each other's algorithms.
+9. Open Coderpad, [repl.it](http://repl.it) or a code editor of your choice.
+10. Using your partner's algorithm, code up a solution to the problem! 
+    1. If you aren't sure what to do based on the algorithm, ask them to modify it instead of hacking and slashing.
+    2. Follow your partner's lead and solve the problem in the way that they prescribed rather than taking your own route.
+
+** *Note that you and your partner will be working on different problems, so it will be essential to provide your partner with a detailed algorithm in order to finish on time ;-D* **
+
+## Algorithm Tips
+
+- Strive to write **language agnostic** algorithms. Instead of planning exactly what method to use when you code, describe what you need to do in plain English. Remember that you may be paired with a student who codes in a different language from you! For example:
+
+```ruby
+# use each_cons to get every 2 elements <- not ideal
+
+# iterate through original array, populating the results array with subarrays containing every combination of two consecutive elements <- good
+```
+
+- Before moving on to the next step, ask yourself if you **know exactly how to execute the step you just wrote**. If there is any fuzziness, stay on the current step and break it down into smaller tasks until you find clarity.
+
+```ruby
+# get all substrings 
+# check if any are in the 2nd arg array
+```
+
+```ruby
+# get all substrings
+	# break string into array of chars
+	# iterate from 0 to index of last character (represents starting index)
+	# iterate from starting idx to index of last character (represents ending index)
+	# take slice of the original str from starting index to ending index
+	# add that slice to results arr
+```
+
+- Feel free to test pieces of your logic in `irb`.
+- When troubleshooting with your partner, ask them to modify their algorithm instead of modifying the code together. Say something like "could you elaborate a little more on line 14?" instead of "what method should I use?"
+
+---
+
+Introductions
+- where in curriculum
+- ruby or javascript
+
+Aryan RB109
+Chris RB109, prep for assessment
+Cory RB130
+Dana JS230
+Elaine RB101
+Felicia RB track
+H
+Marc JS230 Germany
+Mike RB
+Sarah RB109
+Stan RB109 Netherlands
+Sulaiman RB track LS170 Nigeria
+
+write the algorithm in the google doc
+use the approach given to you by your partner
+- ask clarifying questions
+- language agnostic 
+- ruby left / javascript right
+
+
