e1ca24f15b659556b4909fc0019abfa5edd5d443
HyoSung Bidol-Lee <hyosung11@me.com>
2021-08-05 10:00:11 -0400

    Practice Calculate Cat Age exercise

--- Courses/JS100/JavaScript-Basics/Functions-2/calculate_cat_age.js
+++ Courses/JS100/JavaScript-Basics/Functions-2/calculate_cat_age.js
@@ -17,9 +17,17 @@ catAge(3); // 28
 catAge(4); // 32
 */
 
+function catAge(humanYears) {
+  switch (humanYears) {
+    case 0: return 0;
+    case 1: return 15;
+    case 2: return 24;
+    default: return 24 + (humanYears - 2) * 4;
+  }
+}
 
-catAge(0); // 0
-catAge(1); // 15
-catAge(2); // 24
-catAge(3); // 28
-catAge(4); // 32
+console.log(catAge(0)); // 0
+console.log(catAge(1)); // 15
+console.log(catAge(2)); // 24
+console.log(catAge(3)); // 28
+console.log(catAge(4)); // 32
