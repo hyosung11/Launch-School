49c62fe1d8815aa48f22e8cfcaffd415af68b811
HyoSung Bidol-Lee <hyosung11@me.com>
2021-08-08 15:30:56 -0400

    Submit Question 1-2

--- Courses/JS101/Lesson-3-Practice-Problems/easy_1.js
+++ Courses/JS101/Lesson-3-Practice-Problems/easy_1.js
@@ -11,6 +11,29 @@ Will the code below raise an error?
 
 // Bonus
 
-let numbers = [1, 2, 3];
-numbers[6] = 5;
-console.log(numbers[4]); // what will this line return? => undefined
+// let numbers = [1, 2, 3];
+// numbers[6] = 5;
+// console.log(numbers[4]); // what will this line return? => undefined
+
+/* ========
+Question 2
+
+How can you determine whether a given string ends with an exclamation mark(`!`)?
+*/
+
+let str1 = "Come over here!"; // true
+let str2 = "What's up, Doc?"; // false
+
+// function endsWithExclamationMark(string) {
+//   if (string[string.length - 1] === '!') {
+//   return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(endsWithExclamationMark(str1));
+// console.log(endsWithExclamationMark(str2));
+
+console.log(str1.endsWith('!'));
+console.log(str2.endsWith('!'));
