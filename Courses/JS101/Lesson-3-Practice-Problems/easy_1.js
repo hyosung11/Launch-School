233cb51a387b51bdd4b487031daafb6c96d0b437
HyoSung Bidol-Lee <hyosung11@me.com>
2021-08-09 09:39:16 -0400

    Add solution notes to `easy_1.js`

--- Courses/JS101/Lesson-3-Practice-Problems/easy_1.js
+++ Courses/JS101/Lesson-3-Practice-Problems/easy_1.js
@@ -16,6 +16,20 @@ Will the code below raise an error?
 // numbers[6] = 5;
 // console.log(numbers[4]); // what will this line return? => undefined
 
+/* Solution 1
+
+It doesn't raise an error. The JavaScript engine will treat array slots 3 through 5 as empty slots.
+
+Bonus Answer:
+
+numbers[4] will output undefined, but the slot is empty -- it doesn't have a value, not even undefined, in spite of what the line returns. This distinction is important: if you use map(), for instance, the new array will also have empty slots in the corresponding positions. On the other hand, slots with a value of undefined will be remapped based on the return value of the callback:
+*/
+
+let numbers = [1, 2, 3];
+numbers[6] = 5;
+numbers[5] = undefined; // => [ 1, 2, 3, <2 empty items>, undefined, 5 ]
+numbers.map(() => 10);  // => [ 10, 10, 10, <2 empty items>, 10, 10 ]
+
 /* ========
 Question 2
 
@@ -36,6 +50,8 @@ function endsWithExclamationMark(string) {
 console.log(endsWithExclamationMark(str1));
 console.log(endsWithExclamationMark(str2));
 
+/* Solution 2
+
 // console.log(str1.endsWith('!'));
 // console.log(str2.endsWith('!'));
 
@@ -52,6 +68,7 @@ Note that our keys have capitalized names; this usually violates style guideline
 // Hint
 // Read about the Object.prototype.hasOwnProperty() method on MDN.
 
+// Solution 3
 // console.log(ages.hasOwnProperty('Spot'));
 
 /* =======
@@ -62,11 +79,13 @@ Using the following string, create a new string that contains all lowercase lett
 
 // let munstersDescription = "the Munsters are CREEPY and Spooky.";
 
+// Solution 4
+
 // console.log(munstersDescription.charAt(0).toUpperCase() + munstersDescription.substring(1).toLowerCase());
 
 // => The munsters are creepy and spooky.
 
-/* =======
+/* =============================================================================
 Question 5
 
 What will the following code output?
@@ -75,7 +94,17 @@ What will the following code output?
 // console.log(false == '0'); // true
 // console.log(false === '0'); // false
 
-/* =======
+/* Solution 5
+
+The code outputs:
+
+true
+false
+
+In JavaScript, there are two equality operators: strict equality (===) and non-strict (or weak) equality (==). The === operator behaves as a traditional equality operator does in most languages: it evaluates as true when the two expressions on either side have the same type and value. On the other hand, the == operator coerces the values to the same type before comparing them. Coercions can produce unexpected and confusing behavior. Thus, it's good practice to use === rather than ==. The same holds for the !== vs. != operators: prefer !==.
+
+/* =============================================================================
+
 Question 6
 
 We have most of the Munster family in our ages object:
@@ -90,6 +119,8 @@ We have most of the Munster family in our ages object:
 // Hint - did it before seeing this
 // Read about the Object.assign() method on MDN.
 
+// Solution 6
+
 // console.log(Object.assign(ages, additionalAges));
 
 // {
@@ -101,7 +132,8 @@ We have most of the Munster family in our ages object:
 //   Spot: 237
 // }
 
-/* =======
+/* =============================================================================
+
 Question 7
 
 Determine whether the name Dino appears in the strings below -- check each string separately):
@@ -122,7 +154,9 @@ let str2 = 'Fred and Wilma have a pet dinosaur named Dino.';
 // console.log(str1.indexOf('Dino') > - 1); // false
 // console.log(str2.indexOf('Dino') > - 1); // true
 
-/* =======
+
+/* =============================================================================
+
 Question 8
 
 How can we add the family pet, "Dino", to the following array?
@@ -148,7 +182,8 @@ How can we add the family pet, "Dino", to the following array?
 // flintstones[flintstones.length] = "Dino"
 // console.log(flintstones); // => [ 'Fred', 'Barney', 'Wilma', 'Betty', 'Bambam', 'Pebbles', 'Dino' ]
 
-/* =======
+/* =============================================================================
+
 Question 9
 
 In the previous problem, our first answer added 'Dino' to the array like this:
@@ -168,7 +203,8 @@ In the previous problem, our first answer added 'Dino' to the array like this:
 // flintstones.push('Dino', 'Hoppy');  // we can pass multiple arguments to push
 // console.log(flintstones); // => [ 'Fred', 'Barney', 'Wilma', 'Betty', 'Bambam', 'Pebbles', 'Dino', 'Hoppy' ]
 
-/* ========
+/* =============================================================================
+
 Question 10
 
 Return a new version of this sentence that ends just before the word house. Don't worry about spaces or punctuation: remove everything starting from the beginning of house to the end of the sentence.
@@ -176,10 +212,12 @@ Return a new version of this sentence that ends just before the word house. Don'
 
 let advice = "Few things in life are as important as house training your pet dinosaur.";
 
-console.log(advice.slice(0, advice.indexOf('house')));
+/* Hint
 
-// Hint
-// Review the String.prototype.slice() and String.prototype.indexOf() methods on MDN, and use those methods to create the return value.
+Review the String.prototype.slice() and String.prototype.indexOf() methods on MDN, and use those methods to create the return value.
+*/
+
+console.log(advice.slice(0, advice.indexOf('house')));
 
 // Expected return value:
 // => 'Few things in life are as important as '
