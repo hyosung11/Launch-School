c8d02e417774a777ead070d089ffa9ac7c1d93f1
HyoSung Bidol-Lee <hyosung11@me.com>
2021-08-12 15:27:25 -0400

    Answer Question 1

---
+++ Courses/JS101/Lesson-3-Practice-Problems/medium_1.js
@@ -0,0 +1,21 @@
+/* Courses JS101 > Programming Foundations with JavaScript > Lesson 3: Practice Problems > 5. Practice Problems: Medium 1
+
+Practice Problems: Medium 1 */
+
+/* ========================================================
+
+Question 1
+
+Let's do some "ASCII Art": a stone-age form of nerd artwork from back in the days before computers had video screens.
+
+For this practice problem, write a program that outputs The Flintstones Rock! 10 times, with each line indented 1 space to the right of the line above it. The output should start out like this:
+
+The Flintstones Rock!
+ The Flintstones Rock!
+  The Flintstones Rock!
+   ...
+ */
+
+for (let padding = 1; padding <= 10; padding++) {
+  console.log(" ".repeat(padding) + "The Flintstones Rock!");
+}
