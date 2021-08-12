af7ab9580073b8b58b8079491f408c0ab6d81d8e
HyoSung Bidol-Lee <hyosung11@me.com>
2021-08-11 20:27:51 -0400

    Answer Question 8

--- Courses/JS101/Lesson-3-Practice-Problems/easy_2.js
+++ Courses/JS101/Lesson-3-Practice-Problems/easy_2.js
@@ -203,5 +203,13 @@ Question 8
 
 How would you check whether the objects assigned to variables numbers and table below are arrays? */
 
-let numbers = [1, 2, 3, 4]; // true
-let table = { field1: 1, field2: 2, field3: 3, field4: 4 }; // false
+// let numbers = [1, 2, 3, 4]; // true
+// let table = { field1: 1, field2: 2, field3: 3, field4: 4 }; // false
+
+// Hint
+// Read about the Array.isArray() method on MDN.
+
+// console.log(Array.isArray(numbers));
+// console.log(Array.isArray(table));
+
+// Array.isArray() determines whether the passed value is an Array. You can read about this method on MDN.
