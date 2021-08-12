756fe0c1cea798bd36f97f1159260f2e12311b23
HyoSung Bidol-Lee <hyosung11@me.com>
2021-08-12 11:56:01 -0400

    Answer Question 2

--- Courses/JS101/Lesson-3-Practice-Problems/easy_3.js
+++ Courses/JS101/Lesson-3-Practice-Problems/easy_3.js
@@ -8,7 +8,7 @@ Question 1
 
 Write three different ways to remove all of the elements from the following array: */
 
-let numbers = [1, 2, 3, 4];
+// let numbers = [1, 2, 3, 4];
 
 // Solution 1
 // numbers.length = 0;
@@ -19,6 +19,20 @@ let numbers = [1, 2, 3, 4];
 // console.log(numbers);
 
 // Solution 3
-while (numbers.length) {
-  numbers.pop()
-}
+// while (numbers.length) {
+//   numbers.pop();
+// }
+// console.log(numbers);
+
+/* ========================================================
+
+Question 2
+
+What will the following code output? */
+
+console.log([1, 2, 3] + [4, 5]);
+
+// The above code outputs:
+// 1,2,34,5
+
+// In some languages you can use + to concatenate two arrays, but not in JavaScript. In JavaScript, the + operator first converts the arrays to strings, and then concatenates the strings, so the output is the string 1,2,34,5.
