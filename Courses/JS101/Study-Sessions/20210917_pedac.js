97afa71d5e74707ba2e2dc066a9105e387f9e51c
HyoSung Bidol-Lee <hyosung11@me.com>
2021-09-20 05:29:17 -0400

    Add pedac notes

--- Courses/JS101/Study-Sessions/20210917_pedac.js
+++ Courses/JS101/Study-Sessions/20210917_pedac.js
@@ -151,20 +151,19 @@ console.log(palindrome(-4505,15));
 // "Not valid"
 console.log(palindrome(4505,-15));
 // "Not valid"
-*/
 
 /* ====
 
 A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward as forward. Examples of numerical palindromes are:
 
-2332 
-110011 
+2332
+110011
 54322345
-You'll be given 2 numbers as arguments: (num, size). Write a function which returns an array of size number of numerical palindromes that come after num. If num is a palindrome itself, it should be included in the count. 
+You'll be given 2 numbers as arguments: (num, size). Write a function which returns an array of size number of numerical palindromes that come after num. If num is a palindrome itself, it should be included in the count.
 
 Return "Not valid" instead if any one of the inputs is not a number or is less than 0.
 
-Single digit numbers will NOT be considered numerical palindromes. 
+Single digit numbers will NOT be considered numerical palindromes.
 
 
 
