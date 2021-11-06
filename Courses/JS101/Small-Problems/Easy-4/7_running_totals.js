f99d0b00a7dd3ef3e634cc5b79c634cb1ae6dd96
HyoSung Bidol-Lee <hyosung11@me.com>
2021-11-05 22:02:15 -0400

    Submit "Letter Counter (Part 1) solution

--- Courses/JS101/Small-Problems/Easy-4/7_running_totals.js
+++ Courses/JS101/Small-Problems/Easy-4/7_running_totals.js
@@ -1,4 +1,4 @@
-/* JS101 - Small ProblemsEasy 4Running Totals
+/* JS101 - Small Problems > Easy 4 > 7. Running Totals
 
 Running Totals
 
@@ -8,42 +8,55 @@ Write a function that takes an array of numbers and returns an array with the sa
 
 PROBLEM
 Identify expected input and output
-
-- input:
-- output:
+- input: array of numbers
+- output: array with same number of elements
 
 Problem Domain (including implicit requirements)
 
 Make the requirements explicit (clarifying questions)
 
 Identify rules
-
-Mental model of the problem (optional)
+- make the value of each element the running total from the original array
 
 EXAMPLES / TEST CASES
 Validate understanding of the problem
 
 Examples:
+[2, 5, 13] ==> [2, 2 + 5, 2 + 5 + 13] ==> [2, 7, 20]
 
 Edge Cases?
+- `[]`
 
 DATA STRUCTURE
 How we represent the data that we will work with when converting the input to output.
 
-- input
-- intermediary
-- output
+- input: array
+- intermediary: array element transformation
+- output: new array
 
 ALGORITHM
 Steps for converting input to output
+- loop through array
+  - 1st iteration return element at index 0
+  - 2nd iteration return sum of first element + second element
+  - subsequent iteration return total of adding up the elements
 
 CODE
 Implementation of Algorithm
-- test code while programming
+- test code while programming */
 
+// function runningTotal(array) {
+//   let resultArray = [];
+//   let sum = 0;
 
-*/
+//   for (let idx = 0; idx < array.length; idx += 1) {
+//     resultArray.push(sum += array[idx]);
+//   }
 
+//   return resultArray;
+// }
+
+// Laurent's Version?
 // function runningTotal(array) {
 //   let returnedArray = [];
 
@@ -59,15 +72,33 @@ Implementation of Algorithm
 //   return returnedArray;
 // }
 
+// function runningTotal(array) {
+//   let sum = 0;
+
+//   return array.map((number) => {
+//     return sum += number;
+//   });
+// }
+
+// function runningTotal(array) {
+//   let total = 0;
+//   return array.map((num) => total += num);
+// }
+
 function runningTotal(array) {
   let sum = 0;
-  return array.map(number => {
-    sum += number;
-    return sum;
-  });
-
+  return array.map(number => sum += number);
 }
-console.log(runningTotal([2, 5, 13]));             // [2, 7, 20]
-console.log(runningTotal([14, 11, 7, 15, 20]));    // [14, 25, 32, 47, 67]
-console.log(runningTotal([3]));                    // [3]
-console.log(runningTotal([]));                     // []
+
+console.log(runningTotal([2, 5, 13])); // [2, 7, 20]
+console.log(runningTotal([14, 11, 7, 15, 20])); // [14, 25, 32, 47, 67]
+console.log(runningTotal([3])); // [3]
+console.log(runningTotal([])); // []
+
+/* Discussion
+
+The solution makes use of an array and a number variable. The array, `resultArray`, stores the values of the running total, while `sum` keeps track of the current value as we move through each item in the original `array` argument. During each iteration, the solution updates the value of `sum`, incrementing it by the value at the current index of the input `array`.
+
+Further Exploration
+
+Can you rewrite the solution using the `Array.prototype.map` method? What types of problems do you think are well-suited for the `Array.prototype.map` method? */
--- Courses/JS101/Small-Problems/Easy-4/8_letter_counter1.js
+++ Courses/JS101/Small-Problems/Easy-4/8_letter_counter1.js
@@ -1,4 +1,4 @@
-/* JS101 - Small Problems > Easy 4> 8. Letter Counter (Part 1)
+/* JS101 - Small Problems > Easy 4 > 8. Letter Counter (Part 1)
 
 Letter Counter (Part 1)
 
@@ -10,28 +10,20 @@ Words consist of any sequence of non-space characters.
 
 PROBLEM
 Identify expected input and output
-
 - input: string
 - output: object
 
-Problem Domain (including implicit requirements)
-
-Make the requirements explicit (clarifying questions)
-
 Identify rules
-
-Mental model of the problem (optional)
+- evaluate a string of zero or more words
+- return an object which shows key/value pair of size of words and how many words of that size
 
 EXAMPLES / TEST CASES
 Validate understanding of the problem
 
 Examples:
 
-Edge Cases?
-
 DATA STRUCTURE
 How we represent the data that we will work with when converting the input to output.
-
 - input: string
 - intermediary: array
 - output: object
@@ -44,35 +36,64 @@ Steps for converting input to output
   - add a key to the object that represents the value of the number and initialize counter to 1
   - we add 1 to the corresponding key
 4. return the object
+
 CODE
 Implementation of Algorithm
-- test code while programming
+- test code while programming */
 
-*/
+function wordSizes(words) {
+  let count = {};
+  let wordsArray = words.split(' ');
 
-// Examples:
-
-function wordSizes(string) {
-  let returnedObject = {};
-  let numbersArray = string
-    .split(' ')
-    .map(string => string.length);
-
-  if (numbersArray.length === 1 && numbersArray[0] === 0) return returnedObject;
-
-  numbersArray.forEach(element => {
-    if (Object.keys(returnedObject).includes(String(element))) {
-      returnedObject[element] += 1;
-    } else {
-      returnedObject[element] = 1;
+  for (let idx = 0; idx < wordsArray.length; idx += 1) {
+    let wordSize = wordsArray[idx].length;
+    if (wordSize === 0) {
+      continue;
     }
 
-  });
+    if (!count[wordSize]) {
+      count[wordSize] = 0;
+    }
+    count[wordSize] += 1;
+  }
 
-  return returnedObject;
+  return count;
 }
 
+// Examples:
 console.log(wordSizes('Four score and seven.')); // { "3": 1, "4": 1, "5": 1, "6": 1 }
 console.log(wordSizes('Hey diddle diddle, the cat and the fiddle!')); // { "3": 5, "6": 1, "7": 2 }
 console.log(wordSizes("What's up doc?")); // { "2": 1, "4": 1, "6": 1 }
 console.log(wordSizes('')); // {}
+
+/* Discussion
+
+The goal of this exercise is to count the number of words of each size. To do that, the solution first obtains a list of words by splitting the `words` argument string into an array of words. Computing the size of a word is easy, but incrementing the count for that word size is slightly trickier.
+
+The solution stores the counts of word sizes as properties of the `count` object. It does this by iterating over the list of words and performing the following:
+
+1. Initialize the `wordSize` variable to the `length` of the current word.
+2. Check whether `wordSize` is equal to `0`. If yes, proceed immediately to the next iteration since there is no need to account for words of zero length.
+3. Ensure that the property for the current `wordSize` in the `count` object exists and has a value. If the property does not exist (i.e., `count[wordSize]` evaluates to `undefined`), set it to `0`. Otherwise, just set it to its current value. Note that we use bang prefix here. This prefix converts the value to a `boolean` and reverses it.
+4. Increment the count for a particular `wordSize` by `1`. */
+
+
+// Laurent's Version
+// function wordSizes(string) {
+//   let returnedObject = {};
+//   let numbersArray = string
+//     .split(' ')
+//     .map(string => string.length);
+
+//   if (numbersArray.length === 1 && numbersArray[0] === 0) return returnedObject;
+
+//   numbersArray.forEach(element => {
+//     if (Object.keys(returnedObject).includes(String(element))) {
+//       returnedObject[element] += 1;
+//     } else {
+//       returnedObject[element] = 1;
+//     }
+//   });
+
+//   return returnedObject;
+// }
