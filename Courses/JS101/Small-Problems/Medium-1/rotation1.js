c7f4618a6d97c057a31464061bb7d57f4154920a
HyoSung Bidol-Lee <hyosung11@me.com>
2021-11-10 09:24:45 -0500

    Start

--- Courses/JS101/Small-Problems/Medium-1/rotation1.js
+++ Courses/JS101/Small-Problems/Medium-1/rotation1.js
@@ -105,16 +105,16 @@ console.log(rotateArray([7, 3, 5, 2, 9, 1]));       // [3, 5, 2, 9, 1, 7]
 console.log(rotateArray(['a', 'b', 'c']));          // ["b", "c", "a"]
 console.log(rotateArray(['a']));                    // ["a"]
 console.log(rotateArray([1, 'a', 3, 'c']));         // ["a", 3, "c", 1]
-console.log(rotateArray([{ a: 2 }, [1, 2], 3]));    // [[1, 2], 3, { a: 2 }]
+console.log(rotateArray([{ a: 2 }, [1, 2], 3])); // [[1, 2], 3, { a: 2 }]
 console.log(rotateArray([]));                       // []
 
 // return `undefined` if the argument is not an array
-console.log(rotateArray());                         // undefined
-console.log(rotateArray(1));                        // undefined
+console.log(rotateArray()); // undefined
+console.log(rotateArray(1)); // undefined
 
 
 // the input array is not mutated
 let array = [1, 2, 3, 4];
-rotateArray(array);                    // [2, 3, 4, 1]
-console.log(array);                                 // [1, 2, 3, 4]
+rotateArray(array); // [2, 3, 4, 1]
+console.log(array); // [1, 2, 3, 4]
 
