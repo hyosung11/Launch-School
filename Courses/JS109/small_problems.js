ab1b1ef4cab8710a1d216084fc80bf638da33720
HyoSung Bidol-Lee <hyosung11@me.com>
2021-11-19 17:25:49 -0500

    Add update

--- Courses/JS109/small_problems.js
+++ Courses/JS109/small_problems.js
@@ -2753,24 +2753,32 @@ Medium 1 > 2. Rotation (Part 2)
 
 Algo
 - input number and count
-- convert number to string
-- initialiaze `firstPart` of numberString
-- initialize `secondPart` of numberString
+- convert the original `number` to string
+- split the string into two parts
+  - part to remain unchanged
+  - part to be rotated
 - initialize `resultString`
 - initialize `rotate String` helper function
   - return sliced string
-- return number */
+- join the first part back together with the rotated second part
+- return the rotated number */
 
-function rotateRightmostDigits(number, count) {
-  let numberString = String(number);
-  let leftPart = numberString.slice(0, numberString.length - count);
-  let rightPart = numberString.slice(numberString.length - count);
-  console.log(rightPart);
-}
+// function rotateRightmostDigits(number, count) {
+//   let numberString = String(number);
+//   let firstPart = numberString.slice(0, numberString.length - count);
+//   let secondPart = numberString.slice(numberString.length - count);
+//   let resultString = firstPart + rotateString(secondPart);
 
-console.log(rotateRightmostDigits(735291, 1));      // 735291
-console.log(rotateRightmostDigits(735291, 2));      // 735219
-console.log(rotateRightmostDigits(735291, 3));      // 735912
-console.log(rotateRightmostDigits(735291, 4));      // 732915
-console.log(rotateRightmostDigits(735291, 5));      // 752913
-console.log(rotateRightmostDigits(735291, 6));      // 352917
+//   return Number(resultString);
+// }
+
+// function rotateString(string) {
+//   return string.slice(1) + string[0];
+// }
+
+// console.log(rotateRightmostDigits(735291, 1));      // 735291
+// console.log(rotateRightmostDigits(735291, 2));      // 735219
+// console.log(rotateRightmostDigits(735291, 3));      // 735912
+// console.log(rotateRightmostDigits(735291, 4));      // 732915
+// console.log(rotateRightmostDigits(735291, 5));      // 752913
+// console.log(rotateRightmostDigits(735291, 6));      // 352917
