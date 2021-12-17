aecad866f538fb2d475a8e6b96b461e70fe04ea7
HyoSung Bidol-Lee <hyosung11@me.com>
2021-12-17 08:55:04 -0500

    Add study session notes

--- Courses/JS101/Small-Problems/Medium-2/6_sum_square.js
+++ Courses/JS101/Small-Problems/Medium-2/6_sum_square.js
@@ -49,3 +49,50 @@ console.log(sumSquareDifference(100)); // 25164150
 The hardest part of this exercise is figuring out what is meant by the terms "square of the sum" and "sum of the squares". The first example demonstrates the meaning of these two terms by including an extended comment that outlines the process for computing the answer.
 
 The solution shows all the details of this process. It loops through all the integers between `1` and `count`, and computes the `sum` and the `sumOfSquares` as it goes. When the loop finishes, the solution subtracts the `sumOfSquares` from the square of the `sum`, and returns the result. */
+
+/* Sum Square - Square Sum
+
+Write a function that computes the difference between the square of the sum of the first `count` positive integers and the sum of the squares of the first `count` positive integers. 
+
+PROBLEM
+input: number
+output: number
+
+rules:
+-for a given integer input:
+  first add the integers from 1 up to and including that input and then square that sum
+  -second, subtract:
+  each of the integers between 1 and the input individually squared and then added together
+-inputs must be positive
+
+Data structures: numbers
+
+ALGO:
+input: number
+-create squareOfSums and sumOfSquares variables and intiialize to 0
+-while number is greater zero:
+ -add number to squareOfSums
+ -add number squared to sumOfSquares
+-decrement by 1 after each iteration:
+reassign squareOfSums to its value squared
+return squareOfSums - sumOfSquares */
+
+function sumSquareDifference(number) {
+  let squareOfSums = 0;
+  let sumOfSquares = 0;
+
+  while (number > 0) {
+    squareOfSums += number;
+    sumOfSquares += number * number;
+    number -= 1;
+  }
+
+  squareOfSums = squareOfSums * squareOfSums;
+  return squareOfSums - sumOfSquares;
+}
+
+console.log(sumSquareDifference(1)); // 0
+console.log(sumSquareDifference(3)); // 22 --> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
+console.log(sumSquareDifference(4)); // 70 
+console.log(sumSquareDifference(10)); // 2640 */
+console.log(sumSquareDifference(100)); // 25164150
