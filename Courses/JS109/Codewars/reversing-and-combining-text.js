e269133a5c38ff1e46fa828612c26f62da3379fa
HyoSung Bidol-Lee <hyosung11@me.com>
2022-02-02 07:09:25 -0500

    Update

--- Courses/JS109/Codewars/reversing-and-combining-text.js
+++ Courses/JS109/Codewars/reversing-and-combining-text.js
@@ -39,37 +39,81 @@ EXAMPLES
 - 'abc def ghi jkl' --> 'cba fed ihg lkj' -> fedcba
   */
 
-function reverseAndCombineText(str) {
-  let words = str.split(' ').filter((word) => word);
-  // console.log(words); [ 'abc', 'def', 'ghi', 'jkl' ]
+// function reverseAndCombineText(str) {
+//   let words = str.split(' ').filter((word) => word);
+//   // console.log(words); [ 'abc', 'def', 'ghi', 'jkl' ]
 
-  while (words.length > 1) {
-    words = words.map((word) => word.split('').reverse().join(''));
-    // console.log(words)
-    // [ 'cba', 'fed', 'ihg', 'lkj' ]
-    // ['defabc', 'jklghi'];
-    let newWords = [];
-    for (let idx = 0; idx < words.length; idx += 2) {
-      if (idx === words.length - 1) newWords.push(words[idx]);
-      else newWords.push(words[idx] + words[idx + 1]);
+//   while (words.length > 1) {
+//     words = words.map((word) => word.split('').reverse().join(''));
+//     // console.log(words)
+//     // [ 'cba', 'fed', 'ihg', 'lkj' ]
+//     // ['defabc', 'jklghi'];
+//     let newWords = [];
+//     for (let idx = 0; idx < words.length; idx += 2) {
+//       if (idx === words.length - 1) newWords.push(words[idx]);
+//       else newWords.push(words[idx] + words[idx + 1]);
+//     }
+
+//     words = newWords;
+//   }
+
+//   return words[0];
+// }
+
+
+/* Algo
+- input string
+- initialize `words` to an array by splitting the string 
+at each space
+- while the length of `words` is greater than 1, 
+  - iterate by word
+    - split the word into chars
+    - reverse the chars
+    - join the chars
+  - iterate through `words`
+    - concatenate the word at the idx with the word at idx + 1
+      spliced
+- return the first word
+*/
+
+function reverseAndCombineText(str){
+  let words = str.split(' ');
+  // console.log(words)[ 'abc', 'def', 'ghi', 'jkl' ];
+
+  while (words.length > 1) { // [2 elements]
+    words = words
+      .map(word => word
+           .split('')
+           .reverse()
+           .join(''));
+    // console.log(words);
+    // 1st [ 'cba', 'fed', 'ihg', 'lkj' ]
+    // 2nd [ 'defabc', 'jklghi' ]
+
+    for (let idx = 0; idx < words.length; idx++) {
+      // reassign the word at the idx which is 'cba' to the return value of taking 'cba' and concatenating it with 'fed' which is taking the word at idx 1, this 1 element (splice) and combining it with 'cba' + 'fed' => 'cbafed'
+      words[idx] = words[idx].concat(words.splice(idx + 1, 1));
+      // console.log(words[idx]);
+      // cbafed;
+      // ihglkj;
+      // defabcjklghi;
+      // ['cbafed', 'ihglkj']
     }
-
-    words = newWords;
   }
 
-  return words[0];
+  return words[0]; // ['defabcjklghi']
 }
 
-console.log(reverseAndCombineText('dfghrtcbafed') === 'dfghrtcbafed');
+// console.log(reverseAndCombineText('dfghrtcbafed') === 'dfghrtcbafed');
 
-console.log(reverseAndCombineText('abc def') === 'cbafed');
+// console.log(reverseAndCombineText('abc def') === 'cbafed');
 
 console.log(reverseAndCombineText('abc def ghi jkl') === 'defabcjklghi');
 
-console.log(
-  reverseAndCombineText('234hh54 53455 sdfqwzrt rtteetrt hjhjh lllll12  44') ===
-    'trzwqfdstrteettr45hh4325543544hjhjh21lllll'
-);
+// console.log(
+//   reverseAndCombineText('234hh54 53455 sdfqwzrt rtteetrt hjhjh lllll12  44') ===
+//     'trzwqfdstrteettr45hh4325543544hjhjh21lllll'
+// );
 
-console.log(reverseAndCombineText('sdfsdf wee sdffg 342234 ftt') === 'gffds432243fdsfdseewttf'
-);
+// console.log(reverseAndCombineText('sdfsdf wee sdffg 342234 ftt') === 'gffds432243fdsfdseewttf'
+// );
